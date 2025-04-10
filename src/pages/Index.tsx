909b8e0d98106c5a69424fd8daf47cd9672304f3
WonHongLee58 <143569705+LEEWONHONG58@users.noreply.github.com>
2025-04-11 00:05:18 +0900

    main

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import LeftPanel from '@/components/leftpanel/LeftPanel';
 import RightPanel from '@/components/rightpanel/RightPanel';
+import { usePlaces } from '@/hooks/use-places';
+import { useItinerary } from '@/hooks/use-itinerary';
 
 const Index: React.FC = () => {
+  const { filteredPlaces, selectedPlace } = usePlaces();
+  const { itinerary, selectedItineraryDay } = useItinerary();
+
   return (
     <div className="flex h-screen overflow-hidden bg-jeju-light-gray">
       <LeftPanel />
       <RightPanel
-        places={[]}
-        selectedPlace={null}
-        itinerary={null}
-        selectedDay={null}
+        places={filteredPlaces}
+        selectedPlace={selectedPlace}
+        itinerary={itinerary}
+        selectedDay={selectedItineraryDay}
       />
     </div>
   );
